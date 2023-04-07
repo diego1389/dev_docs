3a47960b71817b82709e0ef461e2a0980f99d809
Diego Guillén <dalguillen3089@gmail.com>
2023-04-06 18:27:38 -0600

    Delete Testing with xUnit/mocking-moq-xunit directory

--- Testing with xUnit/mocking-moq-xunit/03/demos/after/07out/CreditCardApplications/ValidationMode.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace CreditCardApplications
-{
-    public enum ValidationMode
-    {
-        Quick,
-        Detailed
-    }
-}
--- Testing with xUnit/mocking-moq-xunit/05/demos/after/01VerifyMethodCall/CreditCardApplications/ValidationMode.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CreditCardApplications
-{
-    public enum ValidationMode
-    {
-        Quick,
-        Detailed
-    }
-}
