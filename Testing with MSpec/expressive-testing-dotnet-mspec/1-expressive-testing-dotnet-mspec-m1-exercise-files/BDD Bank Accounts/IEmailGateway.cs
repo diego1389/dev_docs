842d1f772e318ff793210b74506adc9f6111e425
Diego Guillén <dalguillen3089@gmail.com>
2023-04-06 18:24:53 -0600

    Delete Testing with MSpec/expressive-testing-dotnet-mspec directory

--- Testing with MSpec/expressive-testing-dotnet-mspec/1-expressive-testing-dotnet-mspec-m1-exercise-files/BDD Bank Accounts/IEmailGateway.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace BDD_Bank_Accounts
-{
-    public interface IEmailGateway
-    {
-        void SendEmail(string subject, string from, string to, string message);
-    }
-}
